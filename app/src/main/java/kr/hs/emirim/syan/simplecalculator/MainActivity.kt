e5e3b83e55cf33669843b620e1d4dd999fb6e524
vko-rx <amhdksalsgnl@gmail.com>
2023-05-11 19:29:51 +0900

    심플계산기 완성

--- app/src/main/java/kr/hs/emirim/syan/simplecalculator/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/syan/simplecalculator/MainActivity.kt
@@ -2,10 +2,55 @@ package kr.hs.emirim.syan.simplecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+    lateinit var edit1: EditText;
+    lateinit var edit2: EditText;
+    lateinit var btnAdd: Button;
+    lateinit var btnMinus: Button;
+    lateinit var btnMultiply: Button;
+    lateinit var btnDivide: Button;
+    lateinit var textResult: TextView
+    lateinit var num1: String;
+    lateinit var num2:String
+    var result: Int?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        edit1 = findViewById(R.id.edit1)
+        edit2 = findViewById(R.id.edit2)
+        textResult = findViewById(R.id.text_result)
+        btnAdd = findViewById(R.id.btn_plus)
+        btnMinus = findViewById(R.id.btn_minus)
+        btnMultiply = findViewById(R.id.btn_multiply)
+        btnDivide = findViewById(R.id.btn_divide)
+        btnAdd.setOnClickListener{
+            num1 = edit1.text.toString()
+            num2 = edit2.text.toString()
+            result = Integer.parseInt(num1) + Integer.parseInt(num2)
+            textResult.text = "계산 결과 : " + result
+        }
+        btnMinus.setOnClickListener{
+            num1 = edit1.text.toString()
+            num2 = edit2.text.toString()
+            result = Integer.parseInt(num1) - Integer.parseInt(num2)
+            textResult.text = "계산 결과 : " + result
+        }
+        btnMultiply.setOnClickListener{
+            num1 = edit1.text.toString()
+            num2 = edit2.text.toString()
+            result = Integer.parseInt(num1) * Integer.parseInt(num2)
+            textResult.text = "계산 결과 : " + result
+        }
+        btnDivide.setOnClickListener{
+            num1 = edit1.text.toString()
+            num2 = edit2.text.toString()
+            result = Integer.parseInt(num1) / Integer.parseInt(num2)
+            textResult.text = "계산 결과 : " + result
+        }
     }
 }
